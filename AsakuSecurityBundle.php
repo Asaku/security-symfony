c9be055f8d23ea9d518de7da0fa28ada6f676d44
Mehdi <mehdi@mehdi-aberkane.fr>
2016-10-26 12:11:08 +0200

    composer.json

---
+++ AsakuSecurityBundle.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the AsakuSecurity Bundle
+ *
+ *
+ * Available on github <https://github.com/Asaku/symfony-asaku-security/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Asaku\Security;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ *
+ * @package  AsakuSecurity
+ *
+ * @author   Mehdi Aberkane <mehdi@mehdi-aberkane.fr>
+ * @version  Release: 1.0
+ * @link     https://github.com/Asaku/symfony-asaku-security/
+ *
+ */
+class AsakuSecurityBundle extends Bundle
+{
+
+}
